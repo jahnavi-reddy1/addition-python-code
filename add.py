d98aec4a9dc97481388790a08f6b85f2fa66fce3
jahnavi sai <jahnavisai1234@gmail.com>
2022-04-09 12:16:12 +0530

    Added fibonacci function

--- add.py
+++ add.py
@@ -8,4 +8,17 @@ d = 13
 x = a + b
 y = c*c + a 
 z = x + y + d
-print(x)
+
+#Calculates nth fibonacci number
+#what is fibonacci number f(n) = f(n-1) + f(n-2)
+#e.g. if n = 3 f(3) = f(2) + f(1) => f(3) = f(1) + f(0) + f(1)
+# => f(3) = 1 + 0 + 1 = 2
+# Another example f(5) = f(4) + f(3) => f(3) + f(2) + f(3) => 2 + 1 + 2 => 5
+def fibonacci(n : int) :
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(5))
